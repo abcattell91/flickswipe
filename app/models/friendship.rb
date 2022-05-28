232fbf0b38af5882350090dd3245f8a0be2dd5a4
Alex Cattell <abcattell91@gmail.com>
2022-05-28 13:08:16 +0100

    models friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
+  belongs_to :initiator, class_name: "User"
+  belongs_to :contact, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :initiated_friendships, class_name: "Friendship", foreign_key: "initiator_id"
+  has_many :received_friendships, class_name: "Friendship", foreign_key: "contact_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :friendships
 end
