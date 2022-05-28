2c8a941a0cb62520995fdc55ca24c4483f7510bd
Alex Cattell <abcattell91@gmail.com>
2022-05-28 14:48:29 +0100

    medium.rb removal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_115852) do
+ActiveRecord::Schema.define(version: 2022_05_28_132236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2022_05_28_115852) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["contact_id"], name: "index_friendships_on_contact_id"
     t.index ["initiator_id"], name: "index_friendships_on_initiator_id"
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.float "rating"
+    t.string "genre"
+    t.string "poster"
+    t.string "media_type"
+    t.string "streaming_service"
+    t.integer "duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
