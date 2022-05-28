6322abe668011ea57a1afc16c4bc34337e5b8895
Alex Cattell <abcattell91@gmail.com>
2022-05-28 15:28:46 +0100

    updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :initiated_friendships, class_name: "Friendship", foreign_key: "initiator_id"
   has_many :received_friendships, class_name: "Friendship", foreign_key: "contact_id"
+  has_many :user_contents
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
