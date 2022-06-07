3bda843e10b4af632b1d3a8a821e4204693d642f
Alex Cattell <abcattell91@gmail.com>
2022-06-07 21:38:56 +0100

    fixed iteration over buttons/classlist items and overlay

--- app/javascript/includes/modal.js
+++ app/javascript/includes/modal.js
@@ -2,38 +2,47 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBg = document.querySelectorAll(".movie-modal");
 const modalClose = document.querySelectorAll(".close-button");
 const overlay = document.querySelectorAll(".overlay");
-const overlayClose = document.querySelectorAll("overlay");
+const overlayClose = document.querySelectorAll(".overlay");
+const activeSlide = document.querySelectorAll(".slide .active")
+
+
 
 modalBtn.forEach(function(btn, index){
   btn.addEventListener('click', function() {
-    console.log(index)
-    modalBg.classList.add('active');
-    overlay.classList.add('active');
-    var content_id = activeModal.data("id");
-    console.log(content_id)
+    modalBg.forEach(function(item){
+      item.classList.add('active');
+    });
+    overlay.forEach(function(item){
+      item.classList.add('active');
+    });
   });
 });
 
 modalClose.forEach(function(btn, index){
-  btn.addEventListener('click', function(click) {
-    console.log(click)
-    modalBg.classList.remove('active');
-    overlay.classList.remove('active');
+  btn.addEventListener('click', function() {
+    modalBg.forEach(function(item){
+      item.classList.remove('active');
+    });
+    overlay.forEach(function(item){
+      item.classList.remove('active');
+    });
   });
 });
 
-overlay.forEach(function(overlay, index){
-  overlay.addEventListener('click', function() {
-  overlayClose.classList.remove('active');
-  });
-});
+// overlay.forEach(function(background, index){
+//   background.addEventListener('click', function() {
+//     overlay.forEach(function(item){
+//       item.classList.add('active');
+//     });
+//   });
+// });
 
-overlay.forEach(function(overlay, index){
-  overlay.addEventListener('click', function() {
-  const modals = document.querySelectorAll('.movie-modal.active')
-  modals.forEach(modal => {
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-    })
-  })
-});
+// overlay.forEach(function(overlay, index){
+//   overlay.addEventListener('click', function() {
+//   const modals = document.querySelectorAll('.movie-modal.active')
+//   modals.forEach(modal => {
+//     modal.classList.remove('active');
+//     overlay.classList.remove('active');
+//     })
+//   })
+// });
