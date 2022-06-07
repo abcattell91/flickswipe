48e1d5e281f897f90fd18bef2031267e259d7e81
Alex Cattell <abcattell91@gmail.com>
2022-06-07 19:33:51 +0100

    progress so far. Content controller. Show all posters. Like and Dislike posters/content

--- app/javascript/includes/modal.js
+++ app/javascript/includes/modal.js
@@ -1,28 +1,39 @@
-const modalBtn = document.querySelector(".modal-btn");
-const modalBg = document.getElementById("movie-modal");
-const modalClose = document.querySelector(".close-button");
-const overlay = document.getElementById("overlay");
-const overlayClose = document.getElementById("overlay");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBg = document.querySelectorAll(".movie-modal");
+const modalClose = document.querySelectorAll(".close-button");
+const overlay = document.querySelectorAll(".overlay");
+const overlayClose = document.querySelectorAll("overlay");
 
-modalBtn.addEventListener('click', function(click) {
-  console.log(click)
-  modalBg.classList.add('active');
-  overlay.classList.add('active');
+modalBtn.forEach(function(btn, index){
+  btn.addEventListener('click', function() {
+    console.log(index)
+    modalBg.classList.add('active');
+    overlay.classList.add('active');
+    var content_id = activeModal.data("id");
+    console.log(content_id)
+  });
 });
 
-modalClose.addEventListener('click', function() {
-  modalBg.classList.remove('active');
-  overlay.classList.remove('active');
+modalClose.forEach(function(btn, index){
+  btn.addEventListener('click', function(click) {
+    console.log(click)
+    modalBg.classList.remove('active');
+    overlay.classList.remove('active');
+  });
 });
 
-overlay.addEventListener('click', function() {
+overlay.forEach(function(overlay, index){
+  overlay.addEventListener('click', function() {
   overlayClose.classList.remove('active');
+  });
 });
 
-overlay.addEventListener('click', function() {
+overlay.forEach(function(overlay, index){
+  overlay.addEventListener('click', function() {
   const modals = document.querySelectorAll('.movie-modal.active')
   modals.forEach(modal => {
     modal.classList.remove('active');
     overlay.classList.remove('active');
+    })
   })
-})
+});
