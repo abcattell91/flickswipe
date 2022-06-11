3076c27b990617538ee9d0466c9f338b7e9af9ea
caterina mecheri <cate3rina@hotmail.com>
2022-06-11 15:44:30 +0100

    created matches II

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,6 @@ Rails.application.routes.draw do
     resources :users, :only =>[:show, :index]
   root to: 'pages#home'
 
-  # resources :friendships, only: [:index]
-  # resources :contents, only: [:index] do
-  #   # contents to be nested in friendships?
-  #   # this way once we thumbs up we can link to the 'create' method of matches
-  #   # also need to nest matches create in friendships
-  #   # once we create a match we need the friendship_id which is why it needs to be nested
-  #   # now we go to the matches controller where we can check if our friend liked the same content
-  #   # if they have we create the match
-  #   resources :user_contents, only: [ :create ]
-  # end
-
   # routes 2.0
   resources :users do
     resources :friendships, only: [:index] do
