b0ff40ec6ff237f0e8184c520a02be072519d90f
Alex Cattell <abcattell91@gmail.com>
2022-06-14 21:04:59 +0100

    Finished cloudinary upload and avatar photo for friendships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     @users = User.where.not(id: current_user.id)
 
     @friendships = Friendship.where(initiator: current_user)
+    @friendships_data = @friendships.map do |friendship|
+      { user_photo_key: User.find(friendship.contact_id).photo.key,
+        friendship_id: friendship.id,
+        current_user_friend: User.find(friendship.contact_id),
+        user_first_name: User.find(friendship.contact_id).first_name }
+    end
   end
 
   def index
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,4 @@
 class Friendship < ApplicationRecord
   belongs_to :initiator, class_name: "User"
   belongs_to :contact, class_name: "User"
-
-  # validates :friendship, uniqueness: true
 end
