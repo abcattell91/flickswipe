f6235032886cdc3fd3ec1965a64488aaef994952
Alex Cattell <abcattell91@gmail.com>
2022-06-21 19:33:46 +0100

    added dependent destroy

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ApplicationRecord
-  has_many :content_streaming_services
-  has_many :user_contents
+  has_many :content_streaming_services, dependent: :destroy
+  has_many :user_contents, dependent: :destroy
   has_many :streaming_services, through: :content_streaming_services
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_one_attached :photo
-  has_many :initiated_friendships, class_name: "Friendship", foreign_key: "initiator_id"
-  has_many :received_friendships, class_name: "Friendship", foreign_key: "contact_id"
-  has_many :user_contents
+  has_one_attached :photo, dependent: :destroy
+  has_many :initiated_friendships, class_name: "Friendship", foreign_key: "initiator_id", dependent: :destroy
+  has_many :received_friendships, class_name: "Friendship", foreign_key: "contact_id", dependent: :destroy
+  has_many :user_contents, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
