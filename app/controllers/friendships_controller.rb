91d96655f385c4f4722f559f50432b436f7a6984
Alex Cattell <abcattell91@gmail.com>
2022-06-23 17:51:00 +0100

    Got matched content working along with some styling and linking

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,6 +5,30 @@ class FriendshipsController < ApplicationController
     @friendships = Friendship.where(initiator: current_user)
   end
 
+  def show
+    @friendship = Friendship.find(params[:id])
+
+    @friendships = Friendship.where(initiator: current_user, contact: @friendship.contact_id)
+    @friendship_data = @friendships.map do |friendship|
+      { user_photo_key: User.find(friendship.contact_id).photo.key,
+        friendship_id: friendship.id,
+        current_user_friend: User.find(friendship.contact_id),
+        user_first_name: User.find(friendship.contact_id).first_name }
+      end
+
+    current_user_liked_true = UserContent.where(user_id: current_user, liked: true)
+    friendship_liked_true = UserContent.where(user_id: @friendship.contact_id, liked: true)
+
+    current_user_liked = current_user_liked_true.map(&:content_id).uniq
+
+    friendship_liked = friendship_liked_true.map(&:content_id).uniq
+
+    match_id = current_user_liked & friendship_liked
+
+    @matched = Content.where(id: match_id)
+
+  end
+
   def create
     if Friendship.exists?(friendship_params)
         redirect_to users_path
