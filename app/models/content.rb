3df232f02cee975c10156cde0f5e478d6b1774b8
Alex Cattell <abcattell91@gmail.com>
2022-06-25 19:19:50 +0100

    finished filtering content in matched content by tvshow and movie

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,9 @@ class Content < ApplicationRecord
   has_many :user_contents, dependent: :destroy
   has_many :streaming_services, through: :content_streaming_services
 
-  # scope :filter_by_content, ->(content_type) { where content_type: content_type }
-  # scope :filter_by_rating, ->(rating) { where rating: rating }
+  scope :filter_by_content_movie, ->{ where content_type: "movie" }
+  scope :filter_by_content_tvshow, ->{ where content_type: "tv" }
+  # scope :filter_by_content_rating, ->(rating) { where rating: rating }
+
   # scope :filter_by_duration, ->(duration) { where duration: duration }
 end
