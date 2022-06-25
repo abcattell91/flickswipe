ee4bc377dc096a27a9790cf17569b672e41432a2
Alex Cattell <abcattell91@gmail.com>
2022-06-25 12:50:17 +0100

    rename default error class

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << "alex.local"
+
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
